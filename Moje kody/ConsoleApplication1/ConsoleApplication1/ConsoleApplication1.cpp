2c968fdc747aea45911a7780d20e5b5696273977
Begi <you@example.com>
2025-09-18 15:16:08 +0200

    prvni program

---
+++ Moje kody/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿// ConsoleApplication1.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("ahoj svete");
+
+    return 0;
+
